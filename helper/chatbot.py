e3b0305d5c0b4c8b0f98eae6f77318fce5a3a05d
Esmail Essam <esmailessam6@gmail.com>
2025-01-23 06:54:28 +0200

    add rag

--- helper/chatbot.py
+++ helper/chatbot.py
@@ -1,44 +1,79 @@
 import streamlit as st
 import time
-from .config import client
+from .config import client, embeddings
+import faiss
+import os
+import pickle
+import numpy as np
+
+def search_vdb(user_input:str):
+    
+    index_path = os.path.join(os.getcwd(), 'faiss_data.pkl')
+    
+    with open(index_path, "rb") as f:
+        data = pickle.load(f)
+    index = data["index"]
+    chunks = data["chunks"]
+    
+    query_embedding = embeddings.embed_documents([user_input])
+    
+    distances, indices = index.search(np.array(query_embedding), 5)
+    results = [{"chunk": chunks[i], "distance": distances[0][j]} for j, i in enumerate(indices[0])]
+
+    text = ''
+    for result in results:
+        text += result['chunk']
+
+    return text
+
 
 # Function to integrate LLM like GPT
 def query_llm(client_data=None, guest_mode=None):
     
-    if not guest_mode: 
-        # Create system prompt
-        system_message = f"""
-        You are an assistant helping non-technical employees with their questions using the information provided.
-        
-        Employee information:
-        - Employee ID: {client_data['employee_id']}
-        - Name: {client_data['first_name']} {client_data['last_name']}
-        - Department: {client_data['department_name']}
-        - Position: {client_data['position_name']}
-        - Base Salary: {client_data['base_salary']}
-        - Bonus: {client_data['bonus']}
-        - Hire Date: {client_data['hire_date']}
-        - Performance Rating: {client_data.get('performance_rating', 'N/A')}
-        - Review Period: {client_data.get('review_period', 'N/A')}
-        - Last Review Date: {client_data.get('last_review_date', 'N/A')}
-        - Leave Balance: {client_data.get('annual_leave_balance', 'N/A')} days
-        - Sick Leave Balance: {client_data.get('sick_leave_balance', 'N/A')} days
-        - Last Login: {client_data.get('last_login', 'N/A')}
-        
-        You are NOT ALLOWED to answer any question that is not related to the information provided.
-        """
-    else:
-        # Create system prompt
-        system_message = f"""
-        You are an assistant helping non-technical employees with their questions using the information provided.
-        
-        You are NOT ALLOWED to answer any question that is not related to the information provided.
-        """
-
     # Get user input
     user_input = st.chat_input("What is your question?")
 
     if user_input:
+        context = search_vdb(user_input)
+    
+        if not guest_mode: 
+            # Create system prompt
+            system_message = f"""
+            You are an assistant helping non-technical employees with their questions using the information provided.
+            I have a document that has all the information a bout the company i will provide you with some information related to the user question you have to use it to answer his question
+            You are NOT ALLOWED to answer any question that is not related to the information provided.
+            You have to answer in the same language as the user asked
+            
+            Employee information:
+            - Employee ID: {client_data['employee_id']}
+            - Name: {client_data['first_name']} {client_data['last_name']}
+            - Department: {client_data['department_name']}
+            - Position: {client_data['position_name']}
+            - Base Salary: {client_data['base_salary']}
+            - Bonus: {client_data['bonus']}
+            - Hire Date: {client_data['hire_date']}
+            - Performance Rating: {client_data.get('performance_rating', 'N/A')}
+            - Review Period: {client_data.get('review_period', 'N/A')}
+            - Last Review Date: {client_data.get('last_review_date', 'N/A')}
+            - Leave Balance: {client_data.get('annual_leave_balance', 'N/A')} days
+            - Sick Leave Balance: {client_data.get('sick_leave_balance', 'N/A')} days
+            - Last Login: {client_data.get('last_login', 'N/A')}
+            
+            Company information:
+            - {context}
+            
+            """
+        else:
+            # Create system prompt
+            system_message = f"""
+            You are an assistant helping non-technical user with their questions using the information provided.
+            You are NOT ALLOWED to answer any question that is not related to the information provided.
+            I have a document that has all the information a bout the company i will provide you with some information related to the user question you have to use it to answer his question
+            You have to answer in the same language as the user asked
+            
+            Company information:
+            - {context}
+            """
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": user_input})
         
@@ -75,3 +110,8 @@ def query_llm(client_data=None, guest_mode=None):
         st.session_state.messages.append({"role": "assistant", "content": full_response})
 
     return st.session_state.messages
+
+if __name__ == "__main__":
+    file_path = os.path.join(os.getcwd(), 'handover_index.faiss')
+    print(file_path)
+    print(os.path.exists(file_path))
--- helper/config.py
+++ helper/config.py
@@ -1,5 +1,6 @@
 import os
 from openai import OpenAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 
 
@@ -9,3 +10,8 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # OpenAI Client
 client = OpenAI(api_key=OPENAI_API_KEY)
+
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004",
+                                            google_api_key =GEMINI_API_KEY )
